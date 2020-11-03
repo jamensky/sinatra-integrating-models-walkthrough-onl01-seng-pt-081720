cb56de79447fee2e2078775946d2f78e75b19a45
Jamal Rjoul <jamensky1991@gmail.com>
2020-11-03 21:08:30 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,10 +1,10 @@
 # Your TextAnalyzer model code will go here.
-class TextAnalyzer 
- attr_reader :text 
- 
- def initialize(text)
-   @text = text.downcase 
+class TextAnalyzer
+ attr_reader :text
 
- end 
-   
+ def initialize(text)
+   @text = text.downcase
+
+ end
+
 end 
